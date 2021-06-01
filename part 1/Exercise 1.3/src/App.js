63858f70e3f86a0d76a396f47bf6ec33db677d4a
Jyothsna-123 <69341138+Jyothsna-123@users.noreply.github.com>
2021-06-01 19:53:41 +0530

    Add files via upload

---
+++ part 1/Exercise 1.3/src/App.js
@@ -0,0 +1,27 @@
+const cour = 'Half Stack application development'
+  const jyo1 = 
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const jyo2 = 
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const jyo3 = 
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const total = 
+  {
+    name: 'Total number of exercises'
+  
+  }
+
+       console.log(cour)
+       console.log(jyo1.name , jyo1.exercises)
+       console.log(jyo2.name , jyo2.exercises)
+       console.log(jyo3.name , jyo3.exercises)
+       console.log(total.name , jyo1.exercises + jyo2.exercises + jyo3.exercises)
